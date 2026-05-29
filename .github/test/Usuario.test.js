a92635a3f718afd730494a04d40bfa6be61a8e40
MrRyo117 <164415450+MrRyo117@users.noreply.github.com>
2026-05-28 21:26:19 -0400

    Fix Test Unitarios
    Mejoras y pequeños cambios en el test unitario

--- .github/test/Usuario.test.js
+++ .github/test/Usuario.test.js
@@ -1,4 +1,4 @@
-const Usuario = require(' ../backend/Usuario')
+const Usuario = require('../../src/backend/Usuario')
 
 describe("Pruebas de Usuario ", () =>{
     test('El constructor se asigna correctamente', ()=> {
@@ -12,7 +12,7 @@ describe("Pruebas de Usuario ", () =>{
             estado: true
         };
 
-        const Usuario = new Usuario(
+        const usuario = new Usuario(
             datosEjemplo.id,
             datosEjemplo.nombre,
             datosEjemplo.apellido,
@@ -22,13 +22,13 @@ describe("Pruebas de Usuario ", () =>{
             datosEjemplo.estado
         );
 
-        expect(Usuario.id).toBe(datosEjemplo.id);
-        expect(Usuario.nombre).toBe(datosEjemplo.nombre);
-        expect(Usuario.apellido).toBe(datosEjemplo.apellido);
-        expect(Usuario.correo).toBe(datosEjemplo.correo);
-        expect(Usuario.rol).toBe(datosEjemplo.rol);
-        expect(Usuario.contrasena).toBe(datosEjemplo.contrasena);
-        expect(Usuario.estado).toBe(datosEjemplo.estado);
+        expect(usuario.id).toBe(datosEjemplo.id);
+        expect(usuario.nombre).toBe(datosEjemplo.nombre);
+        expect(usuario.apellido).toBe(datosEjemplo.apellido);
+        expect(usuario.correo).toBe(datosEjemplo.correo);
+        expect(usuario.rol).toBe(datosEjemplo.rol);
+        expect(usuario.contrasena).toBe(datosEjemplo.contrasena);
+        expect(usuario.estado).toBe(datosEjemplo.estado);
         expect(typeof usuario.estado).toBe('boolean')
 
     });
